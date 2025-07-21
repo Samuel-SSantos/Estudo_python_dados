43406625b58a1fb40956d85e17cf2775f13126c3
Samuel Santos <samueel.ssilva@gmail.com>
2025-07-21 00:43:02 -0300

    Update first.py

--- first.py
+++ first.py
@@ -1,4 +1,9 @@
-tarefa = input("Digite uma tarefa: ")
+while True:
+  opcao = input("Digite uma tarefa (ou 'sair' para finalizar): ")
+  if opcao.lower() == 'sair':
+    break
+tarefa = opcao
 
-print("Tarefa foi adicionada!")
-print("Tarefas: ", tarefa)
+print("Tarefa adicionada: ", tarefa)
+
+print("ultima tarefa adicionada: ", tarefa)
