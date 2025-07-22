a102f9afb66e111dcf62e503b8879e06d8a88d1e
Samuel Santos <samueel.ssilva@gmail.com>
2025-07-22 03:02:43 -0300

    Update first.py

--- first.py
+++ first.py
@@ -1,11 +1,32 @@
 while True:
-    opcao = input("Digite uma tarefa (ou 'sair' para finalizar): ")
-    if opcao.lower() == 'sair':
-        break
-  
-    tarefas.append (opcao)
-    print("Tarefa adicionada: ", opcao)
+    print("** Menu de Opções **")
+    print("1. Adicionar Tarefas")
+    print("2. Listar Tarefas")
+    print("3. Remover Tarefas")
+    print("4. Sair")
 
-print('Lista de Tarefas: ')
-for tarefa in tarefas:
-    print('-', tarefa)
+    opcao = input("Escolha uma opção: ")
+    if opcao == '1':
+        tarefa = input("Digite uma tarefa: ")
+        tarefas.append(tarefa)
+        print("Tarefa adiconada: ", tarefa)
+
+    elif opcao == '2':
+        print("Lista de tarefas: ")
+        for tarefa in tarefas:
+            print("-", tarefa)
+
+    elif opcao == '3':
+        tarefa = input ("Digite a tarefa a ser removida: ")
+        if tarefa in tarefas:
+            tarefas.remove(tarefa) 
+            print("Tarefa removida: ", tarefa)
+        else: 
+            print("Tarefa não encontrada")
+
+    elif opcao == '4':
+        print("Saindo...")
+        break
+    
+    else:
+        print("Opção invalida!")1
