8a0c52c3faf323a212244f3d4ed6d90b4f8e70fd
Samuel Santos <samueel.ssilva@gmail.com>
2025-07-23 01:50:25 -0300

    Update first.py

--- first.py
+++ first.py
@@ -9,12 +9,11 @@ def adicionar_tarefa(nome, prioridade):
     print("Tarefa adiconada: ", tarefa)
 
 def listar_tarefas():
-    print("Lista de tarefas: ")
+    print("Lista de tarefas:")
     for tarefa in tarefas:
         print(f"- {tarefa['nome']} (prioridade {tarefa['prioridade']})")
 
 def remover_tarefas(bone):
-    nome = input ("Digite o nome da tarefa a ser removida: ")
     for tarefa in tarefas:
         if tarefa ['nome'] == nome:
             tarefas.remove(tarefa) 
@@ -24,30 +23,30 @@ def remover_tarefas(bone):
             print("Tarefa não encontrada")
 
 
-    while True:
-        print("** Menu de Opções **")
-        print("1. Adicionar Tarefas")
-        print("2. Listar Tarefas")
-        print("3. Remover Tarefas")
-        print("4. Sair")
+while True:
+    print("** Menu de Opções **")
+    print("1. Adicionar Tarefas")
+    print("2. Listar Tarefas")
+    print("3. Remover Tarefas")
+    print("4. Sair")
 
-        opcao = input("Escolha uma opção: ")
-        if opcao == '1':
-            nome = input("Digite o nome da tarefa: ")
-            prioridade = input("Digite a prioridade da tarefa: ")
+    opcao = input("Escolha uma opção: ")
+    if opcao == '1':
+        nome = input("Digite o nome da tarefa: ")
+        prioridade = input("Digite a prioridade da tarefa: ")
 
-            adicionar_tarefa(nome, prioridade)
+        adicionar_tarefa(nome, prioridade)
 
-        elif opcao == '2':
-            listar_tarefas()
+    elif opcao == '2':
+        listar_tarefas()
 
-        elif opcao == '3':
-            nome - input("Digite o nome da tarefa a ser removida: ")
-            remover_tarefas(nome)
+    elif opcao == '3':
+        nome = input("Digite o nome da tarefa a ser removida: ")
+        remover_tarefas(nome)
 
-        elif opcao == '4':
-            print("Saindo...")
-        break
+    elif opcao == '4':
+         print("Saindo...")
+    break
     
-    else:
-        print("Opção invalida!")
+else:
+    print("Opção invalida!")
