f8fea7ddd0d9094ecb9f1fc0d587398c828569aa
Samuel Santos <samueel.ssilva@gmail.com>
2025-07-23 01:39:35 -0300

    Update first.py

--- first.py
+++ first.py
@@ -1,40 +1,52 @@
-while True:
-    print("** Menu de Opções **")
-    print("1. Adicionar Tarefas")
-    print("2. Listar Tarefas")
-    print("3. Remover Tarefas")
-    print("4. Sair")
+tarefas = []
+def adicionar_tarefa(nome, prioridade):
+    tarefa = {
+        'nome' : nome,
+        'prioridade' : prioridade
+    }
 
-    opcao = input("Escolha uma opção: ")
-    if opcao == '1':
-        nome = input("Digite o nome da tarefa: ")
-        prioridade = input("Digite a prioridade da tarefa: ")
+    tarefas.append(tarefa)
+    print("Tarefa adiconada: ", tarefa)
 
-        tarefa = {
-            'nome' = nome,
-            'prioridade' = prioridade
-        }
+def listar_tarefas():
+    print("Lista de tarefas: ")
+    for tarefa in tarefas:
+        print(f"- {tarefa['nome']} (prioridade {tarefa['prioridade']})")
 
-        tarefas.append(tarefa)
-        print("Tarefa adiconada: ", tarefa)
-
-    elif opcao == '2':
-        print("Lista de tarefas: ")
-        for tarefa in tarefas:
-            print(f"- {tarefa['nome']} (prioridade {tarefa['prioridade']})")
-
-    elif opcao == '3':
-        nome = input ("Digite o nome da tarefa a ser removida: ")
-        for tarefa in tarefas:
-            if tarefa ['nome'] == nome:
+def remover_tarefas(bone):
+    nome = input ("Digite o nome da tarefa a ser removida: ")
+    for tarefa in tarefas:
+        if tarefa ['nome'] == nome:
             tarefas.remove(tarefa) 
             print("Tarefa removida: ", nome)
             break
         else: 
             print("Tarefa não encontrada")
 
-    elif opcao == '4':
-        print("Saindo...")
+
+    while True:
+        print("** Menu de Opções **")
+        print("1. Adicionar Tarefas")
+        print("2. Listar Tarefas")
+        print("3. Remover Tarefas")
+        print("4. Sair")
+
+        opcao = input("Escolha uma opção: ")
+        if opcao == '1':
+            nome = input("Digite o nome da tarefa: ")
+            prioridade = input("Digite a prioridade da tarefa: ")
+
+            adicionar_tarefa(nome, prioridade)
+
+        elif opcao == '2':
+            listar_tarefas()
+
+        elif opcao == '3':
+            nome - input("Digite o nome da tarefa a ser removida: ")
+            remover_tarefas(nome)
+
+        elif opcao == '4':
+            print("Saindo...")
         break
     
     else:
